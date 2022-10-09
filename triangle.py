261d977af89382cd6ef3542c1e59ffe708f7f0a1
YOGESH <yogeshverma02349@gmail.com>
2022-10-09 09:19:24 +0530

    just adding it

---
+++ triangle.py
@@ -0,0 +1,8 @@
+#to find out the area of triangle
+i=8
+while i>7:
+
+    h=float(input('enter height of your traingle:'))
+    b=float(input('enter the base of your triangle:'))
+    area= (b*h*0.5)
+    print ('the area of your triangle is:',(area))
