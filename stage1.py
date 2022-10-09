3e8a7b6e28e7c208f9ef1c1735d91aec1fc470ff
Yogesh-JI <114800021+Yogesh-JI@users.noreply.github.com>
2022-10-09 10:46:05 +0530

    Delete stage1.py

--- stage1.py
+++
@@ -1,8 +0,0 @@
-print("this is a code to add any two integers")
-i=10
-while i>0:
-    x=int(input("please enter the first digit:"))
-    y=int(input(" please enter the second digit:"))
-    c=x+y
-    print("your desired result is:",(c))
-
