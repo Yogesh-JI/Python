3090079453370f9d337a45db6ac982f8606292b5
YOGESH <yogeshverma02349@gmail.com>
2022-10-11 19:02:09 +0530

    complete

---
+++ learning condition 1.py
@@ -0,0 +1,10 @@
+i=9
+while i>8:
+    x=int(input('enter a lenght:'))
+    y=int(input('enter the width:'))
+    if x==y :
+        print('its a square')
+    else:
+
+        print("its a rectangle")    
+    
