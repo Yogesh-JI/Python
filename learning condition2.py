e0fc1cbeb870fb86cb3aa94fc5e96b86af495a8e
YOGESH <yogeshverma02349@gmail.com>
2022-10-12 23:03:08 +0530

    complete

---
+++ learning condition2.py
@@ -0,0 +1,9 @@
+i=9
+while i>8:
+    x=int(input('enter the salary :'))
+    y=int(input('enter the year of service:'))
+    if y>5 :
+        print('the net bonus amount is:',(x+1000))
+    else:
+      print("sorry no bonus for you")    
+    
