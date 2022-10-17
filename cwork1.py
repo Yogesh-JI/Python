e1bed3995121dd8c1d2a093556779649d0aaa913
YOGESH <yogeshverma02349@gmail.com>
2022-10-17 22:35:52 +0530

    learning

---
+++ cwork1.py
@@ -0,0 +1,13 @@
+a=int(input())
+b=int(input())
+c=int(input())
+if (a+b)>c or (b+c)>a or(c+a)>b:
+    print('trianlge is possible')
+else:
+    print ('you cant make triangle')   
+    
+
+
+    
+
+ 
