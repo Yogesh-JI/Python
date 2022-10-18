c42d2811c6ca3eead0fa929371bd06bb02713a10
YOGESH <yogeshverma02349@gmail.com>
2022-10-19 02:05:42 +0530

    complete

---
+++ sum of n terms.py
@@ -0,0 +1,8 @@
+#to find out sum of n terms ,starting with 1
+i=2
+while i>1:
+    a=int(input('enter the number of terms:'))
+    c=a+1
+    k=float(a/2)
+    h=int(c*k)
+    print('sum of given n terms is:',(h))
