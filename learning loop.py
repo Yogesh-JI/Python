65c64c5b30b88ceba5423b3981d40a3e2a0610e3
YOGESH <yogeshverma02349@gmail.com>
2022-11-04 22:12:30 +0530

    learning

---
+++ learning loop.py
@@ -0,0 +1,14 @@
+i=11
+while i>1:
+    i-=1
+  #  if i==90:
+   #     continue
+    print(i)
+#the program will run till else condition in while loop
+#else:
+ #   print('vdfnzdk')    
+   
+    
+                           # if i==70:
+                           #   break
+    
