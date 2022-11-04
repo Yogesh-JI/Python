783b474d4ef1ff3a18d23bd3cb8dda8af220d640
YOGESH <yogeshverma02349@gmail.com>
2022-11-04 22:13:42 +0530

    learning

---
+++ for loop.py
@@ -0,0 +1,10 @@
+car=['tata','mahindra','bmw','thar','ford']
+#for i in car:
+#    if i=='bmw':
+#        continue
+#    print(i)
+#for i in 'hello world':
+#    print (i)
+i=0
+while i in car:
+    print(i)
