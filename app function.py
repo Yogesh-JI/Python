9ca783f8c6a62ac2ee4d400308008124150abe1d
YOGESH <yogeshverma02349@gmail.com>
2022-11-09 21:31:52 +0530

    que. of def

---
+++ app function.py
@@ -0,0 +1,11 @@
+def det(*fr):
+    for i in fr:
+        print(i)
+det('yogi',1046546,64,64,654,98,498,49,84,98)
+
+
+
+def rich(work='labour',age=20): #  GIVING SOME DEFAULT VALUES
+    print('my work is:'+work,age)
+rich('hardwork',45)     
+rich()
