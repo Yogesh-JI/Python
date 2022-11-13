8cb069cba58d9ca6888513d0c6b6c855bb8e61b2
YOGESH <yogeshverma02349@gmail.com>
2022-11-13 16:13:46 +0530

    updated

--- task3.py
+++ task3.py
@@ -1,4 +1,32 @@
-a=(input('a:'))
-b=(input('b:'))
-c=int(a)+int(b)
-print('a='+a+',b='+b+',sum='+str(c))
+# a=(input('a:'))
+# b=(input('b:'))
+# c=int(a)+int(b)
+# print('a='+a+',b='+b+',sum='+str(c))
+
+
+
+# n=[1,2,3,4,5]
+# nl=[]
+# for i in n:
+#     g=i**2
+#     nl.append(g)
+# print(nl)    
+
+
+
+# n=[1,2,3,4,5,2,6]
+# for i in n:
+#     if i==2:
+#         g=n.index(i)
+#         n.remove(i)
+#         n.insert(g,200)
+#         break
+# print(n)    
+
+
+
+l1=['x','y','z']
+l2=[1,2,3]
+l1.extend(l2)
+print(l1)
+
