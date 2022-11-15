efee10289817d5e33e6c1b2f74eaf495afb02f58
YOGESH <yogeshverma02349@gmail.com>
2022-11-15 22:29:01 +0530

    love

---
+++ love.py
@@ -0,0 +1,23 @@
+# b=''
+# d=''
+# for i in range(5):
+#     b+=str(i)
+
+# for j in b:
+#     c=b.index(j)+1
+#     d+=j*c
+# print(d)   
+
+
+
+from turtle import *
+color("red")
+begin_fill()
+pensize(6)
+left(50)
+forward(133)
+circle(50,200)
+right(140)
+circle(50,200)
+forward(133)
+end_fill()
