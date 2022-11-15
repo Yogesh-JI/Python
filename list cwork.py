4159a840ec59895e6615d37cbffa22fc49fe2f21
YOGESH <yogeshverma02349@gmail.com>
2022-11-15 22:32:47 +0530

    claas work on list

---
+++ list cwork.py
@@ -0,0 +1,31 @@
+colors=['red','blue','red','black']
+# colors[0:2]='yellow','orange'
+# print(colors)
+cars=['tata','nano','hundai','alto','jeep']
+nl=[]
+
+
+
+# while x< len(colors):
+#     print(cars[x])
+#     x+=1
+
+
+
+
+# for i in cars:
+#     if 'a' in i:
+#         nl.append(i)
+
+# print(nl)
+
+
+
+# nl=[x for x in cars if 'a' in x]             #FUCKING IMPORTANT
+# print(nl)
+
+
+
+
+nl=[x.upper() for x in cars]
+print(nl)
