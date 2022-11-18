8617d49bb50baafde11eb833dbec26d6fa48fe16
YOGESH <yogeshverma02349@gmail.com>
2022-11-18 23:14:18 +0530

    update

--- help.py
+++ help.py
@@ -1,11 +1,6 @@
-h=str(input())
-g=str(input())
-
-o=(h.index(g))+1
-if o in range(len(h)):
-        print(h[o])
-else:
-    print('not found')        
-        
+h=int(input())
+g=int(input())
+for i in range(h,g+1):
+  
     
         
