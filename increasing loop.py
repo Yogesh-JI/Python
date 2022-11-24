ee40a11a928ba4b27b0ca201f39cf1f8d462c732
YOGESH <yogeshverma02349@gmail.com>
2022-11-24 23:29:40 +0530

    it is

--- increasing loop.py
+++ increasing loop.py
@@ -1,7 +1,9 @@
-n=int(input('enter the range:'))
-s=''
-for i in range(2,n+1):
-    for j in range(i-1):
+j=int(input('enter the range:'))
+def loop(n):
+    s=''
+    for i in range(2,n+1):
+       for j in range(i-1):
         s+=str(j+2)+' '
-    print(s)
-    s=''    
+       print(s)
+       s=''    
+loop(j)
