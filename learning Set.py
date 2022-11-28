630677cf08b1fc77366e8b13c673ddb27715a226
YOGESH <yogeshverma02349@gmail.com>
2022-11-28 21:54:57 +0530

    it is

---
+++ learning Set.py
@@ -0,0 +1,18 @@
+# unordered
+# immutable(some Exception)
+# duplication is not allowed
+
+
+
+mset={'car','boat','pool'}
+for i in mset:
+    if i=='boat':
+        print(i)
+        break
+
+
+tople1=('H','E','L','L','O')
+s=''
+for i in tople1:
+    s+=i
+print(s)    
