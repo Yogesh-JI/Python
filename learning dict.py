49b6f550e8ffe06db8dee2d48c6f5de46d83d0fe
YOGESH <yogeshverma02349@gmail.com>
2022-12-11 21:15:56 +0530

    added some new things

---
+++ learning dict.py
@@ -0,0 +1,21 @@
+h={
+    'name':'yogesh',
+    'age':19,
+    'percentage':89.9,
+    'year':2022
+}
+print(h.keys())
+print(h.items())
+print(h.values())
+
+h['age']=34
+print(h['age'])
+
+h.update({'age':45})
+print(h)
+#h.pop('age')
+print(h) 
+
+
+print(h.keys())
+print(h.values())
