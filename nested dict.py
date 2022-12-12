7c0108247acccc45f9016064cf077964c58fb0e9
YOGESH <yogeshverma02349@gmail.com>
2022-12-12 23:22:40 +0530

    just it

---
+++ nested dict.py
@@ -0,0 +1,13 @@
+my= {
+    "class":{
+        "student":{
+            "name" : "abc",
+            "marks" : {
+                "python" : 90,
+                "web" : 95
+            }
+        }
+    }
+}
+
+print(my['class']['student']['marks']['web'])
