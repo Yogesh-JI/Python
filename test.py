14f44e63cb6ad503b0ab78eac84b6158685dc730
YOGESH <yogeshverma02349@gmail.com>
2023-01-08 21:16:31 +0530

    just it

--- test.py
+++ test.py
@@ -1,19 +1,19 @@
-a=int(input())
-b=int(input())
-def ram(a,b,h):
-    o=(a+b)
-    print('a+=b:',(o),'a=',a,'b=',b)
-ram(a,b,0)
-def rbm(e,f):
-    g=a-b
-    print('a-=b:',g,'a=',a,'b=',b)
-rbm(a,b)    
-def rcm(i,j):
-    h=a*b
-    print('a*=b:',h,'a=',a,'b=',b)
-rcm(a,b)    
-def rdm(k,l):
-    m=a/b
-    print('a/=b:',m,'a=',a,'b=',b)
-rdm(a,b)    
+# a=int(input())
+# b=int(input())
+# def ram(a,b,h):
+#     o=(a+b)
+#     print('a+=b:',(o),'a=',a,'b=',b)
+# ram(a,b,0)
+# def rbm(e,f):
+#     g=a-b
+#     print('a-=b:',g,'a=',a,'b=',b)
+# rbm(a,b)    
+# def rcm(i,j):
+#     h=a*b
+#     print('a*=b:',h,'a=',a,'b=',b)
+# rcm(a,b)    
+# def rdm(k,l):
+#     m=a/b
+#     print('a/=b:',m,'a=',a,'b=',b)
+# rdm(a,b)    
 
