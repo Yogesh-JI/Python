5c274d7cf76678b3469e14401b984895ad2c6d1f
YOGESH <yogeshverma02349@gmail.com>
2023-02-03 20:25:01 +0530

    just it

---
+++ answers.py
@@ -0,0 +1,14 @@
+# l=list(map(int,input().split()))
+# c2,c3,c5,c7=0,0,0,0
+# for i in l:
+   
+
+# a=[int(x) for x in input().split]
+l=[]
+while True:
+    a=input()
+    if a=='stop' or a=='STOP':
+       break
+    else:
+        l.append(int(a))
+print(l)
