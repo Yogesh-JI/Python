a39dee1a16b45e0ab63c67baf5aaa5a549e5f4aa
YOGESH <yogeshverma02349@gmail.com>
2023-02-06 19:43:45 +0530

    just it

--- factorial01.py
+++ factorial01.py
@@ -1,6 +1,17 @@
-def fact(n):
-    if n==1:
-        return 1
-    return n*fact(n-1)    
-num=fact(8)   
-print(num) 
+# def fact(n):
+#     if n==1:
+#         return 1
+#     else:
+#         return n*fact(n-1)    
+# num=fact(8)   
+# print(num) 
+import math as m
+s=int(input())
+r=[]
+for i in range(s+1):
+    c=0
+    for j in str(i):
+        c+=m.factorial(int(j))
+    if c==i:
+        r.append(i)
+print(r)
