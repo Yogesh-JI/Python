1006add0500b70ba27158aea1154f2a418bc871a
Yogesh-JI <yogeshverma02349@gmai.com>
2023-03-31 23:49:38 +0530

    more accurative

--- app function.py
+++ app function.py
@@ -6,6 +6,6 @@ det('yogi',1046546,64,64,654,98,498,49,84,98)
 
 
 def rich(work='labour',age=20): #  GIVING SOME DEFAULT VALUES
-    print('my work is:'+work,age)
+    print('my work is '+work,age)
 rich('hardwork',45)     
 rich()
