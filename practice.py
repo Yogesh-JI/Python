220a43087dd2d44c01b8cf8d25b17c560c369c9e
Yogesh-JI <yogeshverma02349@gmai.com>
2023-04-03 21:02:23 +0530

    just more practice

--- practice.py
+++ practice.py
@@ -48,13 +48,13 @@
 # print(fact(num))
 
 
-def fibonacci(n):
-    # Initialize the first two Fibonacci numbers
-    fib = [0, 1]
+# def fibonacci(n):
+#     # Initialize the first two Fibonacci numbers
+#     fib = [0, 1]
     
-    # Calculate the remaining Fibonacci numbers
-    for i in range(2, n):
-        fib.append(fib[i-1] + fib[i-2])
-    return fib
+#     # Calculate the remaining Fibonacci numbers
+#     for i in range(2, n):
+#         fib.append(fib[i-1] + fib[i-2])
+#     return fib
 
-print(fibonacci(5))
+# print(fibonacci(5))
